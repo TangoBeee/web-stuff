2d8a76502d874e4c72371f43838556cb1cb206e1
TangoBeee <zonebattle90@gmail.com>
2023-11-07 02:55:26 +0530

    font-end part is done

---
+++ simple-typing-test-website/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let main = document.getElementById('main');
+let textContainer = document.getElementById('text-container');
+let resultsContainer = document.getElementById('result');
+let wpmText = document.getElementById('wpm');
+let accuracyText = document.getElementById('accuracy');
+let timeText = document.getElementById('time');
+
